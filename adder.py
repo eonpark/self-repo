962566391a0f60f6352536e1b3f132546f440247
eonpark <pocva6243@gmail.com>
2024-01-12 17:13:31 +0900

    fix: Rename Function add to adder

--- adder.py
+++ adder.py
@@ -1,7 +1,7 @@
-def add(num1:int,num2:int)->int:
+def adder(num1:int,num2:int)->int:
 	return int(num1)+int(num2)
 
 if __name__=='__main__':
 	uinput1=input('Enter your num1: ')
 	uinput2=input('Enter your num2: ')
-	print(add(uinput1,uinput2))
+	print(adder(uinput1,uinput2))
